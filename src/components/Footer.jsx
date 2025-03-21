33a5abaf12ee87c7748ad64b16d569b3a966631e
slami96 <adam.slamen@gmail.com>
2025-03-21 19:46:56 +0100

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <p>Movie Recommendation Expert System &copy; {new Date().getFullYear()}</p>
+        <p>Created for Educational Purposes</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
